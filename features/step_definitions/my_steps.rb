98288f8b965fc07e6a4618cb9f4b13eea51b6dab
kleer <levidt@hotmail.com>
2018-11-30 10:38:58 -0500

    se agrego vista index y vista juego

---
+++ features/step_definitions/my_steps.rb
@@ -0,0 +1,19 @@
+Given(/^que abri el juego$/) do
+    visit('/')
+  end
+  
+  When(/^click en "([^"]*)"$/) do |arg1|
+    click_button("Comenzar")
+  end
+  
+  Then(/^debo ver "([^"]*)"$/) do |arg1|
+    "La matriz ha sido creada"
+  end
+  
+  When(/^se realizo el acomodo de la nave$/) do
+    "La nave ha sido ubicada"
+  end
+  
+  Then(/^la nave debe ocupar una sola coordenada$/) do
+    "La matriz tiene una posición ocupadasss"
+  end
