887b756dfadfc719d250fbc658370cb134794784
kleer <levidt@hotmail.com>
2018-11-30 12:53:47 -0500

    Se genera el numero de coordenada y se convierte a binario

--- lib/batalla-galactica.rb
+++ lib/batalla-galactica.rb
@@ -1,28 +1,19 @@
 class BatallaGalactica
     
     def initialize
-        @tablero = {}
         @posicion_nave = 0
     end
 
     def crea_tablero
-        @tablero = {
-            "0,0,0"=>0,
-            "0,0,1"=>1,
-            "0,1,0"=>2,
-            "0,1,1"=>3,
-            "1,0,0"=>4,
-            "1,0,1"=>5,
-            "1,1,0"=>6,
-            "1,1,1"=>7,
-        }
-        "La matriz de 2x2x2 ha sido creada"
     end
 
     def crea_nave
-        @posicion_nave = rand(8)
-        "La nave ha sido colocada en 1,1,1"
+        @posicion_nave = rand(8).to_s(2)
+        "La nave ha sido colocada exitosamente"
     end
 
+    def coordenadas_nave
+        @posicion_nave
+    end
 
 end
--- spec/batalla-galactica_spec.rb
+++ spec/batalla-galactica_spec.rb
@@ -1,15 +1,17 @@
 require './lib/batalla-galactica'
 
 describe BatallaGalactica do
-    it "Inicia Juego" do
-        bg = BatallaGalactica.new
-        resutado = bg.crea_tablero
-        expect(resutado).to eq ("La matriz de 2x2x2 ha sido creada")
-    end
-    
+
     it "Crea nave" do
         bg = BatallaGalactica.new
         resutado = bg.crea_nave
-        expect(resutado).to eq ("La nave ha sido colocada en 1,1,1")
+        expect(resutado).to eq ("La nave ha sido colocada exitosamente")
+    end
+
+    it "La nave debe tener coordenadas en binario" do
+        bg = BatallaGalactica.new
+        bg.crea_nave
+        coordenadas1 = bg.coordenadas_nave
+        expect(coordenadas1).to match /[01]+/m
     end
 end
