66752fbd59a4580f65e6d2ff88f89a49548e63d1
kleer <levidt@hotmail.com>
2018-11-30 10:44:46 -0500

    se agrega modelo inicial, metodo crear_tablero y endpoint post para juego

--- app.rb
+++ app.rb
@@ -4,3 +4,9 @@ require './config'
 get '/' do
     erb(:index)
 end
+
+post '/juego' do
+    bg = BatallaGalactica.new
+    bg.crea_tablero
+    erb(:juego)
+end
---
+++ batalla-galactica.rb
@@ -0,0 +1,8 @@
+class BatallaGalactica
+
+    def crea_tablero
+        "La matriz de 2x2x2 ha sido creada"
+    end
+
+
+end
